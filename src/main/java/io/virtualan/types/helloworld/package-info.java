633fb8e5a610a38f0051a8b52d1e43a0e05a715d
Elan Thangamani <elans3.java@gmail.com>
2020-09-23 22:44:18 -0500

    Added with sales force IOT

--- src/main/java/io/virtualan/types/helloworld/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://virtualan.io/types/helloworld", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package io.virtualan.types.helloworld;
